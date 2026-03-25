2704f5d5595c98c3b83ddc8492c3f4b0b67fa415
Daniel Torres <danieltorresg13@gmail.com>
2026-03-24 19:06:04 -0500

    add brain-even game and demo

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,25 @@
+from brain_games.cli import welcome_user
+import random
+import prompt 
+
+def main():
+    name = welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for game in range(3):
+        num = random.randint(1, 100)
+        print(f"Question: {num}")
+        answer = prompt.string("Your answer: ")
+        if num % 2 == 0:
+            correct_answer = "yes"
+        else:
+            correct_answer = "no" 
+    
+        if answer == correct_answer:
+            print("Correct!")
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            break
+    else:
+        print(f"Congratulations, {name}!")
+
