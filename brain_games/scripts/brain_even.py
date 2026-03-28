6301022b5aa7dc654c669acd02546b322ee379cd
Daniel Torres <danieltorresg13@gmail.com>
2026-03-28 17:20:58 -0500

    Fix Sonar issue in brain-even engine

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,25 +1,14 @@
-from brain_games.cli import welcome_user
+from brain_games.engine import run_game
 import random
-import prompt 
+
+
+def generate_round():
+    num = random.randint(1, 100)
+    question = str(num)
+    correct_answer = "yes" if num % 2 == 0 else "no"
+    return question, correct_answer
+
 
 def main():
-    name = welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    for _ in range(3):
-        num = random.randint(1, 100)
-        print(f"Question: {num}")
-        answer = prompt.string("Your answer: ")
-        if num % 2 == 0:
-            correct_answer = "yes"
-        else:
-            correct_answer = "no" 
-    
-        if answer == correct_answer:
-            print("Correct!")
-        else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            break
-    else:
-        print(f"Congratulations, {name}!")
-
+    rules = 'Answer "yes" if the number is even, otherwise answer "no".'
+    run_game(generate_round, rules)
