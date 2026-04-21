596564a9513fe0f2ada0291c82c671fe57f16dd5
Daniel Torres <danieltorresg13@gmail.com>
2026-04-20 22:23:00 -0500

    Fix spaces brain_even

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -3,6 +3,7 @@ import random
 MIN_NUMBER = 1
 MAX_NUMBER = 100
 
+
 def generate_round():
     num = random.randint(MIN_NUMBER, MAX_NUMBER)
     question = str(num)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,6 +1,7 @@
 from brain_games.engine import run_game
 from brain_games.games.brain_even import generate_round
 
+
 def main():
     rules = 'Answer "yes" if the number is even, otherwise answer "no".'
     run_game(generate_round, rules)
